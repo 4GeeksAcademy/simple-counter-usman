3fe288024bbd0f5f0f6bb576d1893bd52dc647fd
Usman257 <117535587+Usman257@users.noreply.github.com>
2024-06-04 18:35:32 +0000

    terminado

--- src/js/component/simpleCounter.jsx
+++ src/js/component/simpleCounter.jsx
@@ -44,19 +44,20 @@ const SimpleCounter = (props) => {
 
 	return (
 		<>
-		<div className="ccounter ">
-		<div className="clock">
+		
+		<div className="ccounter d-flex justify-content-center d-flex justify-content-around ">
+		<div className="clock ">
 			<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
 				<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
 				<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
 			</svg>
 		</div>
 		<div className="six"><h1>{digitSix}</h1></div>
-		<div className="five">{digitFive}</div>
-		<div className="four">{digitFour}</div>
-		<div className="tree">{digitThree}</div>
-		<div className="two">{digitTwo}</div>
-		<div className="one">{digitOne}</div>
+		<div className="five"><h1>{digitFive}</h1></div>
+		<div className="four"><h1>{digitFour}</h1></div>
+		<div className="tree"><h1>{digitThree}</h1></div>
+		<div className="two"><h1>{digitTwo}</h1></div>
+		<div className="one"><h1>{digitOne}</h1></div>
 	</div>
 </>
 		
